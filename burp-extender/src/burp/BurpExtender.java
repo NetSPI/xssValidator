c4c6d79e55cb62b29dec30c2dacb4c60ee50e94c
John <Poulin@Johns-MacBook-Pro.local>
2014-01-14 23:09:02 -0500

    Basic intruder extender functional. Sends payloads to host as approperiate. Doesn't parse, yet.

--- burp-extender/src/burp/BurpExtender.java
+++ burp-extender/src/burp/BurpExtender.java
@@ -1,13 +1,74 @@
 package burp;
 import burp.*;
 
-public class BurpExtender implements IBurpExtender	{
+public class BurpExtender implements IBurpExtender, IIntruderPayloadGeneratorFactory, IIntruderPayloadProcessor	{
 	public burp.IBurpExtenderCallbacks mCallbacks;
+	private IExtensionHelpers helpers;
+	
+	public static final byte[][] PAYLOADS = {
+		"|".getBytes(),
+		"<script>alert(1)</script>".getBytes()
+	};
+	
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 		mCallbacks = callbacks;
+		
+		helpers = callbacks.getHelpers();
+		callbacks.setExtensionName("XSS Auditor Payloads");
+		
+		callbacks.registerIntruderPayloadGeneratorFactory(this);
+		callbacks.registerIntruderPayloadProcessor(this);
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("test");
+	@Override
+	public String getGeneratorName() {
+		return "XSS Auditor Payloads";
+	}
+	
+	@Override
+    public IIntruderPayloadGenerator createNewInstance(IIntruderAttack attack)
+    {
+        // return a new IIntruderPayloadGenerator to generate payloads for this attack
+        return new IntruderPayloadGenerator();
+    }
+
+    //
+    // implement IIntruderPayloadProcessor
+    //
+    
+    @Override
+    public String getProcessorName()
+    {
+        return "XSS Validator";
+    }
+    
+    @Override
+    public byte[] processPayload(byte[] currentPayload, byte[] originalPayload, byte[] baseValue) {
+    	return helpers.stringToBytes(helpers.urlEncode(helpers.bytesToString(currentPayload)));
+    }
+		
+	class IntruderPayloadGenerator implements IIntruderPayloadGenerator {
+		int payloadIndex;
+		
+		@Override
+		public boolean hasMorePayloads() {
+			System.out.println("Checking for more payloadz");
+			return payloadIndex < PAYLOADS.length;
+		}
+		
+		@Override
+		public byte[] getNextPayload(byte[] baseValue) {
+			System.out.println("Getting next payload");
+			
+			byte[] payload = PAYLOADS[payloadIndex];
+			payloadIndex++;
+			return payload;
+		}
+		
+		@Override
+		public void reset() {
+			payloadIndex = 0;
+		}
+		
 	}
 }
