78e7ad19f6a0c0e75549d1e2ff8511852fed38f0
Mike McCabe <mccabe615@gmail.com>
2014-02-03 22:52:39 -0500

    adding gitignore and x.js for dynamic js

---
+++ x.js
@@ -0,0 +1,13 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+Usage:
+
+var payload= getUrlVars()["payload"];
+var method= getUrlVars()["method"];
+
+alert(payload);
