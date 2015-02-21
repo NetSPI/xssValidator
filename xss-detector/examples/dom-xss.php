96c9ee5f95daa3a4ecb227c17c6940753d924157
John Poulin <john.m.poulin@gmail.com>
2015-02-21 15:52:33 -0500

    Removed outdated examples and slimer.js script

--- xss-detector/examples/dom-xss.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-Hello
-<script>
-var pos=document.URL.indexOf("context=")+8; 
-document.write(decodeURIComponent(document.URL.substring(pos,document.URL.length)));
-</script>
-</body>
-</html>
